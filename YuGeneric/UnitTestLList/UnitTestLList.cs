2bf39b08c9a8cca5b6fd5a516d443e8ce894ba33
feedfedfat <scottszb1987@hotmail.com>
2015-05-22 18:22:43 +1200

    v0.10
    - LList<T> added - a generic linked list, try to provide the major
    methods provided by the official LinkedList<T>

---
+++ YuGeneric/UnitTestLList/UnitTestLList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestLList
+    {
+        [TestMethod]
+        public void LListConstructor()
+        {
+            var lListString = new LList<string>();
+            var lListInt = new LList<int>();
+            var lListBool = new LList<bool>();
+        }
+    }
+}
---
+++ YuGeneric/YuGeneric/LList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGeneric
+{
+    public class LList<T>
+    {
+        public class Node<T>
+        {
+            public T Value;
+            public Node<T> Next;
+        }
+
+        private Node<T> _head;
+
+        public void AddFirst(Node<T> node)
+        {
+            node.Next = _head;
+            _head = node;
+        }
+
+        public override string ToString()
+        {
+            Node<T> current = _head;
+            string result = "";
+
+            while (current != null)
+            {
+                result += current.Value.ToString();
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        public int Count()
+        {
+            Node<T> current = _head;
+            int count = 0;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+
+            return count;
+        }
+    }
+}
