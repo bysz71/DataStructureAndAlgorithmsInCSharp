97a6dcd9f97cc517f329b65788251696721ebce2
feedfedfat <scottszb1987@hotmail.com>
2015-05-25 13:34:30 +1200

    V0.100
    -A generic linked list implemented
    -it is a singly linked list, with a _head pointer and a _tail pointer
    -implemented methods:
    -AddBefore(Node<T> , Node<T>)
    -AddBefore(Node<T> , T)
    -AddAfter(Node<T> , Node<T>)
    -AddAfter(Node<T> , T)
    -AddFirst(Node<T>)
    -AddFirst(T)
    -AddLast(Node<T>)
    -AddLast(T)
    -Clear()
    -Contains(T)
    -Equals(LList<T>)
    -First
    -Last
    -Find(T)
    -FindLast(T)
    -Remove(T)
    -Remove(Node<T>)
    -RemoveFirst()
    -RemoveLast()
    -ToString()
    -Not sure how to implement Enumerable<T> interface,
    thus cannnot use foreach()

--- YuGeneric/UnitTestLList/UnitTestLList.cs
+++ YuGeneric/UnitTestLList/UnitTestLList.cs
@@ -15,5 +15,216 @@ namespace UnitTestYuGeneric
             var lListInt = new LList<int>();
             var lListBool = new LList<bool>();
         }
+
+        [TestMethod]
+        public void LListFirst()
+        {
+            var listVoid = new LList<int>();
+            var listNotVoid = new LList<int>();
+            listNotVoid.AddFirst(2000);
+
+            Assert.ReferenceEquals(listVoid.First, null);
+            Assert.AreEqual(listNotVoid.First.Value, 2000);
+        }
+
+        [TestMethod]
+        public void LListLast()
+        {
+            var listVoid = new LList<int>();
+            var listOne = new LList<int>();
+            listOne.AddFirst(100);
+            var listTwo = new LList<int>();
+            listTwo.AddFirst(10);
+            listTwo.AddFirst(20);
+
+            Assert.ReferenceEquals(listVoid.Last, null);
+            Assert.AreEqual(listOne.Last.Value, 100);
+            Assert.AreEqual(listTwo.Last.Value, 10);
+        }
+
+        [TestMethod]
+        public void LListAddFirst()
+        {
+            var list1 = new LList<string>();
+            var list2 = new LList<string>();
+
+            var node1 = new LListNode<string>("node1");
+            var node2 = new LListNode<string>("node2");
+
+            list1.AddFirst("111");
+            list1.AddFirst("222");
+
+            list2.AddFirst(node1);
+            list2.AddFirst(node2);
+
+            Console.WriteLine("List1: " + list1.ToString());
+            Console.WriteLine("List2: " + list2.ToString());
+        }
+
+        [TestMethod]
+        public void LListAddLast()
+        {
+            var list1 = new LList<int>();
+            list1.AddLast(1);
+            list1.AddLast(2);
+        //    Console.WriteLine("list1: " + list1.ToString());
+
+        //    var list2 = new LList<int>();
+        //    var node1 = new LListNode<int>(1);
+        //    var node2 = new LListNode<int>(2);
+        //    list2.AddLast(node1);
+        //    list2.AddLast(node2);
+        //    Console.WriteLine("list2: " + list2.ToString());
+        }
+
+        [TestMethod]
+        public void LListFind()
+        {
+            var list = new LList<int>();
+            for (int i = 0; i < 10; i++)
+                list.AddLast(i);
+            var node = list.Find(3);
+            Assert.AreEqual(node.Value, 3);
+
+            var list2 = new LList<string>();
+            list2.AddFirst("a");
+            list2.AddFirst("b");
+            list2.AddFirst("c");
+            var node2 = list2.Find("b");
+            Assert.AreEqual(node2.Value, "b");
+
+            var list3 = new LList<int>();
+            var node3 = list3.Find(2);
+            Assert.AreEqual(node3, null);
+        }
+
+        [TestMethod]
+        public void LListFindLast()
+        {
+            var list = new LList<int>();
+            list.AddLast(1);
+            list.AddLast(1);
+            list.AddLast(2);
+            var node = list.FindLast(1);
+            Assert.AreEqual(node.Next.Value, 2);
+
+            var list2 = new LList<int>();
+            list2.AddFirst(1);
+            Assert.AreEqual(list2.FindLast(1).Value, 1);
+        }
+
+        [TestMethod]
+        public void LListAddAfter()
+        {
+            var list = new LList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+            var node = list.Find(2);
+            list.AddAfter(node, new LListNode<int>(4));
+            list.AddAfter(list.Find(3), 5);
+            Console.WriteLine(list.ToString() + "the _tail pointer is now: " + list.Last.Value);
+        }
+
+        [TestMethod]
+        public void LListAddBefore()
+        {
+            var list = new LList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+            list.AddBefore(list.Find(2), new LListNode<int>(4));
+            list.AddBefore(list.First, 5);
+            Console.WriteLine(list.ToString() + "the _head pointer is now: " + list.First.Value);
+        }
+        
+        [TestMethod]
+        public void LListRemove()
+        {
+            var list = new LList<int>();
+            for (int i = 0; i < 10; i++)
+                list.AddLast(i);
+            list.AddLast(100);
+            list.AddLast(100);
+            Console.WriteLine(list.ToString());
+            list.Remove(100);
+            Console.WriteLine(list.ToString());
+            list.RemoveFirst();
+            Console.WriteLine(list.ToString());
+            list.RemoveLast();
+            Console.WriteLine(list.ToString());
+
+            var list2 = new LList<int>();
+            list2.AddLast(1);
+            list2.RemoveFirst();
+            Assert.ReferenceEquals(list2.First, list2.Last);
+            Assert.ReferenceEquals(list2.First, null);
+
+            var list3 = new LList<int>();
+            list3.AddLast(1);
+            list3.AddLast(2);
+            list3.RemoveLast();
+            Assert.ReferenceEquals(list3.First, list3.Last);
+        }
+
+        [TestMethod]
+        public void LListCount()
+        {
+            var list = new LList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            Assert.AreEqual(list.Count(), 2);
+            list.RemoveLast();
+            Assert.AreEqual(list.Count(), 1);
+            list.RemoveLast();
+            Assert.AreEqual(list.Count(), 0);
+        }
+
+        [TestMethod]
+        public void LListContains()
+        {
+            var list = new LList<int>();
+            list.AddLast(10);
+            if (list.Contains(10))
+                Console.WriteLine("list contains 10");
+            else
+                Console.WriteLine("list does not contain 10");
+
+            var list2 = new LList<string>();
+            list2.AddLast("asdf");
+            if(list2.Contains("asdf"))
+                Console.WriteLine("list contains asdf");
+            else
+                Console.WriteLine("list does not contain asdf");
+            if (list2.Contains("aaa"))
+                Console.WriteLine("list contains aaa");
+            else
+                Console.WriteLine("list does not contain aaa");
+        }
+
+        [TestMethod]
+        public void LListToString()
+        {
+            var list = new LList<int>();
+            Console.WriteLine(list.ToString());
+            list.AddLast(10);
+            Console.WriteLine(list.ToString());
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddLast(i);
+            }
+            Console.WriteLine(list.ToString());
+        }
+
+        [TestMethod]
+        public void LListClear()
+        {
+            var list = new LList<int>();
+            for (int i = 0; i < 10; i++)
+                list.AddLast(i);
+            list.Clear();
+            Assert.ReferenceEquals(list, null);
+            Assert.AreEqual(list.Count(), 0);
+        }
     }
 }
--- YuGeneric/YuGeneric/LList.cs
+++ YuGeneric/YuGeneric/LList.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿//My version of generic singly linked list, LList contains both head and tail pointer
+//it provides better performance than only head pointer in most circumstances
+//will implement:
+//AddBefore(Node<T> , Node<T>)*
+//AddBefore(Node<T> , T)*
+//AddAfter(Node<T>,Node<T>)*
+//AddAfter(Node<T>,T)*
+//AddFirst(T)*
+//AddFirst(Node<T>)*
+//AddLast(T)*
+//AddLast(Node<T>)*
+//Clear*
+//Contains(T)*
+//Equals*
+//First*
+//Find*
+//FindLast*
+//Last*
+//Remove(T)*
+//Remove(Node<T>)*
+//RemoveFirst*
+//RemoveLast*
+//ToString*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,39 +29,371 @@ using System.Threading.Tasks;
 
 namespace YuGeneric
 {
+    public class LListNode<T>
+    {
+        public T Value;
+        public LListNode<T> Next;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public LListNode()
+        {
+        }
+
+        /// <summary>
+        /// Constructor with initial value
+        /// </summary>
+        /// <param name="value"></param>
+        public LListNode(T value)
+        {
+            Value = value;
+        }
+    }
+
     public class LList<T>
     {
-        public class Node<T>
+        private LListNode<T> _head;
+        private LListNode<T> _tail;
+
+        /// <summary>
+        /// Geter of the first item of the LList
+        /// </summary>
+        public LListNode<T> First
         {
-            public T Value;
-            public Node<T> Next;
+            get
+            {
+                if (_head == null)
+                    return null;
+                else
+                    return _head;
+            }
+            internal set
+            {
+                _head = value;
+            }
+        }
+        /// <summary>
+        /// Geter of the last item
+        /// </summary>
+        public LListNode<T> Last
+        {
+            get
+            {
+                if (_tail == null)
+                {
+                    return null;
+                }
+                return _tail;
+            }
+            internal set
+            {
+                _tail = value;
+            }
         }
 
-        private Node<T> _head;
-
-        public void AddFirst(Node<T> node)
+        /// <summary>
+        /// Add a LList Node to the front
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddFirst(LListNode<T> node)
         {
-            node.Next = _head;
-            _head = node;
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+            else
+            {
+                node.Next = _head;
+                _head = node;
+            }
         }
 
-        public override string ToString()
+        /// <summary>
+        /// Add value as a node to the front
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddFirst(T value)
         {
-            Node<T> current = _head;
-            string result = "";
+            var node = new LListNode<T>(value);
+            AddFirst(node);
+        }
+
+        /// <summary>
+        /// Add a node to the rear
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddLast(LListNode<T> node)
+        {
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+            else
+            {
+                _tail.Next = node;
+                _tail = node;
+            }
+        }
+
+        /// <summary>
+        /// Add a value as a node to the rear
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddLast(T value)
+        {
+            var node = new LListNode<T>(value);
+            AddLast(node);
+        }
+
+        /// <summary>
+        /// Add a node after a specific node
+        /// both cannot be null
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="newNode"></param>
+        public void AddAfter(LListNode<T> node, LListNode<T> newNode)
+        {
+            if (node == _tail)
+            {
+                AddLast(newNode);
+            }
+            else
+            {
+                newNode.Next = node.Next;
+                node.Next = newNode;
+            }
+        }
+
+        /// <summary>
+        /// Add a value as a node after a specific node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="value"></param>
+        public void AddAfter(LListNode<T> node, T value)
+        {
+            var newNode = new LListNode<T>(value);
+            AddAfter(node, newNode);
+        }
+
+        /// <summary>
+        /// Add a node before specific node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="newNode"></param>
+        public void AddBefore(LListNode<T> node, LListNode<T> newNode)
+        {
+            if (node == _head)
+            {
+                AddFirst(newNode);
+                return;
+            }
+            var current = _head;
+            while (current.Next != node)
+            {
+                current = current.Next;
+            }
+            newNode.Next = node;
+            current.Next = newNode;
+        }
+
+        /// <summary>
+        /// Add a value as a node before specific node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="value"></param>
+        public void AddBefore(LListNode<T> node, T value)
+        {
+            var newNode = new LListNode<T>(value);
+            AddBefore(node, newNode);
+        }
+
+
+        /// <summary>
+        /// Clear this LList
+        /// </summary>
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        /// <summary>
+        /// find the first appearence of specific value and return the node
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public LListNode<T> Find(T value)
+        {
+            var current = _head;
+
+            if (current == null)
+                return null;
 
             while (current != null)
             {
-                result += current.Value.ToString();
+                if (EqualityComparer<T>.Default.Equals(current.Value, value))
+                {
+                    return current;
+                }
                 current = current.Next;
             }
 
-            return result;
+            return null;
         }
 
+        /// <summary>
+        /// Find the last appearance of a value and return the node
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public LListNode<T> FindLast(T value)
+        {
+            if (_head == null)
+                return null;
+
+            var current = _head;
+            LListNode<T> buffer = null;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Value, value))
+                {
+                    buffer = current;
+                }
+                current = current.Next;
+            }
+
+            return buffer;
+        }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <returns></returns>
+        public bool Equals(LList<T> list)
+        {
+            if (Object.ReferenceEquals(this.First, list.First))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Remove the object with first appearance of a value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Remove(T value)
+        {
+            if (_head == null)
+                return false;
+            var current = _head;
+            while (current.Next != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Next.Value, value))
+                {
+                    current.Next = current.Next.Next;
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Remove specific node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public bool Remove(LListNode<T> node)
+        {
+            if (_head == null)
+                return false;
+
+            var current = _head;
+            while (current.Next != null)
+            {
+                if (Object.ReferenceEquals(current.Next, node))
+                {
+                    if (current.Next == _tail)
+                    {
+                        _tail = current;
+                        current.Next = null;
+                    }
+                    else
+                    {
+                        current.Next = current.Next.Next;
+                    }
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// remove the head of list
+        /// </summary>
+        /// <returns></returns>
+        public bool RemoveFirst()
+        {
+            if (_head == null)
+                return false;
+
+            if (_head.Next == null)
+            {
+                _head = null;
+                _tail = null;
+                return true;
+            }
+
+            _head = _head.Next;
+            return true;
+        }
+
+        /// <summary>
+        /// remove the tail of the list
+        /// </summary>
+        /// <returns></returns>
+        public bool RemoveLast()
+        {
+            if (_head == null)
+                return false;
+
+            if (_head.Next == null)
+            {
+                _head = null;
+                _tail = null;
+                return true;
+            }
+
+            var current = _head;
+            while (current.Next.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = null;
+            _tail = current;
+            return true;
+        }
+
+        /// <summary>
+        /// Return the number of items in the list
+        /// </summary>
+        /// <returns></returns>
         public int Count()
         {
-            Node<T> current = _head;
+            LListNode<T> current = _head;
             int count = 0;
             while (current != null)
             {
@@ -47,6 +402,49 @@ namespace YuGeneric
             }
 
             return count;
+        }
+
+        /// <summary>
+        /// Find out if the list contains an item with some specific value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Contains(T value)
+        {
+            if (_head == null)
+                return false;
+
+            var current = _head;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Value, value))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Override from System.Object, convert the list to a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            LListNode<T> current = _head;
+            string result = "";
+
+            while (current != null)
+            {
+                result += current.Value.ToString();
+                if(current.Next !=null)
+                    result += ",";
+                current = current.Next;
+            }
+
+            return result;
         }
     }
 }
