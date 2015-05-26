3c3e9026b2e45e7b5c19c6bae21acb0ed734ffd4
feedfedfat <scottszb1987@hotmail.com>
2015-05-26 22:56:12 +1200

    V0.20
    -Implemented Stack, using singly linked list
    -provide Push() Pop() Top and IsEmpty()

---
+++ YuGeneric/UnitTestLList/UnitTestStack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestStack
+    {
+        [TestMethod]
+        public void StackNodeConstructor()
+        {
+            var node1 = new StackNode<int>();
+            var node2 = new StackNode<string>();
+
+            var node3 = new StackNode<int>(10);
+            var node4 = new StackNode<string>("aaa");
+        }
+
+        [TestMethod]
+        public void StackNodeProperty()
+        {
+            var node1 = new StackNode<int>(10);
+            Assert.AreEqual(node1.Value, 10);
+            Assert.ReferenceEquals(node1.Next, null);
+        }
+
+        [TestMethod]
+        public void StackConstructor()
+        {
+            var stack = new Stack<int>();
+            var stack2 = new Stack<string>();
+        }
+
+        [TestMethod]
+        public void StackTop()
+        {
+            var stack = new Stack<int>();
+            Assert.ReferenceEquals(stack.Top, null);
+
+            stack.Push(10);
+            stack.Push(20);
+            Assert.AreEqual(stack.Top.Value, 20);
+            stack.Top.Value = 30;
+            Assert.AreEqual(stack.Top.Value, 30);
+        }
+
+        [TestMethod]
+        public void StackPush()
+        {
+            var stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(10);
+            var node = new StackNode<int>(100);
+            stack.Push(node);
+        }
+
+        [TestMethod]
+        public void StackPop()
+        {
+            var stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Pop();
+            Assert.AreEqual(stack.Top.Value, 10);
+        }
+
+        [TestMethod]
+        public void StackIsEmpty()
+        {
+            var stack = new Stack<int>();
+            Assert.AreEqual(stack.IsEmpty(), true);
+            stack.Push(10);
+            Assert.AreEqual(stack.IsEmpty(), false);
+        }
+    }
+}
---
+++ YuGeneric/YuGeneric/Stack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGeneric
+{
+    public class Stack<T>
+    {
+        private StackNode<T> _top;
+
+        public StackNode<T> Top
+        {
+            get
+            {
+                return _top;
+            }
+            set
+            {
+                _top = value;
+            }
+        }
+
+        public void Push(StackNode<T> node)
+        {
+            if (_top == null)
+                _top = node;
+            else
+            {
+                node.Next = _top;
+                _top = node;
+            }
+        }
+
+        public void Push(T value)
+        {
+            var node = new StackNode<T>(value);
+            this.Push(node);
+        }
+
+        public void Pop()
+        {
+            if (_top == null)
+                return;
+            else
+            {
+                _top = _top.Next;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (_top == null)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class StackNode<T>
+    {
+        private T _value;
+        private StackNode<T> _next;
+
+        public StackNode()
+        {
+        }
+
+        public StackNode(T value){
+            _value = value;
+            _next = null;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public StackNode<T> Next
+        {
+            get
+            {
+                return _next;
+            }
+            set
+            {
+                _next = value;
+            }
+        }
+    }
+}
