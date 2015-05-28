caff7c96bc96c93721c38c07aefaee5ec94a969c
feedfedfat <scottszb1987@hotmail.com>
2015-05-28 12:32:45 +1200

    V0.30
    -Generic Queue implemented
    -using singly linked list

---
+++ YuGeneric/UnitTestLList/UnitTestQueue.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestQueue
+    {
+        [TestMethod]
+        public void QueueNodeConstructor()
+        {
+            var node1 = new QueueNode<int>();
+            var node2 = new QueueNode<string>("aaa");
+        }
+
+        [TestMethod]
+        public void QueueNodeProperty()
+        {
+            var node = new QueueNode<string>("bbb");
+            Assert.AreEqual(node.Value, "bbb");
+            Assert.ReferenceEquals(node.Next, null);
+
+            var node2 = new QueueNode<string>("ccc");
+            node2.Next = node;
+            Assert.ReferenceEquals(node2.Next, node);
+        }
+
+        [TestMethod]
+        public void QueueConstructor()
+        {
+            var qInt = new Queue<int>();
+            var qString = new Queue<string>();
+        }
+
+        [TestMethod]
+        public void QueueProperty()
+        {
+            var queue = new Queue<int>();
+            Assert.ReferenceEquals(queue.Front, null);
+            Assert.ReferenceEquals(queue.Rear, null);
+
+            queue.Join(10);
+            Assert.AreEqual(queue.Front.Value, 10);
+            Assert.AreEqual(queue.Rear.Value, 10);
+
+            queue.Join(20);
+            Assert.AreEqual(queue.Front.Value, 20);
+            Assert.AreEqual(queue.Rear.Value, 10);
+        }
+
+        [TestMethod]
+        public void QueueCount()
+        {
+            var queue = new Queue<int>();
+            Assert.AreEqual(queue.Count(), 0);
+
+            queue.Join(0);
+            Assert.AreEqual(queue.Count(), 1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Join(i);
+            }
+            Assert.AreEqual(queue.Count(), 11);
+        }
+
+        [TestMethod]
+        public void QueueIsEmpty()
+        {
+            var queue = new Queue<int>();
+            Assert.AreEqual(queue.IsEmpty(), true);
+            queue.Join(10);
+            Assert.AreEqual(queue.IsEmpty(), false);
+        }
+
+        [TestMethod]
+        public void QueueJoin()
+        {
+            var queue = new Queue<int>();
+            queue.Join(new QueueNode<int>(10));
+            Assert.AreEqual(queue.Front.Value, 10);
+            queue.Join(100);
+            Assert.AreEqual(queue.Front.Value, 100);
+            Assert.AreEqual(queue.Count(), 2);
+        }
+
+        [TestMethod]
+        public void QueueLeave()
+        {
+            var queue = new Queue<int>();
+            Assert.AreEqual(queue.Leave(), false);
+            queue.Join(10);
+            Assert.AreEqual(queue.Leave(), true);
+            Assert.AreEqual(queue.IsEmpty(), true);
+            queue.Join(20);
+            queue.Join(30);
+            Assert.AreEqual(queue.Leave(), true);
+            Assert.AreEqual(queue.Count(), 1);
+            Assert.AreEqual(queue.Front.Value, 20);
+        }
+    }
+}
---
+++ YuGeneric/YuGeneric/Queue.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGeneric
+{
+    public class QueueNode<T>
+    {
+        private T _value;
+        private QueueNode<T> _next;
+
+        public QueueNode(){
+        }
+
+        public QueueNode(T value)
+        {
+            _value = value;
+        }
+
+        public T Value{
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public QueueNode<T> Next
+        {
+            get
+            {
+                return _next;
+            }
+            set
+            {
+                _next = value;
+            }
+        }
+    }
+
+    public class Queue<T> 
+    {
+        private QueueNode<T> _front;
+        private QueueNode<T> _rear;
+
+        public QueueNode<T> Front
+        {
+            get
+            {
+                return _front;
+            }
+            set
+            {
+                _front = value;
+            }
+        }
+
+        public QueueNode<T> Rear
+        {
+            get
+            {
+                return _rear;
+            }
+            set
+            {
+                _rear = value;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (_front == null && _rear == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            var current = _front;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+
+        public void Join(QueueNode<T> node)
+        {
+            if (this.IsEmpty())
+            {
+                _front = node;
+                _rear = node;
+            }
+            else
+            {
+                node.Next = _front;
+                _front = node;
+            }
+        }
+
+        public void Join(T value)
+        {
+            this.Join(new QueueNode<T>(value));
+        }
+
+        public bool Leave()
+        {
+            if (this.IsEmpty())
+            {
+                return false;
+            }
+            else if(this.Count() == 1)
+            {
+                _front = null;
+                _rear = null;
+                return true;
+            }
+            else
+            {
+                _front = _front.Next;
+                return true;
+            }
+        }
+    }
+}
