aacd905a26497175b9dc9e793b636c6d443460ee
feedfedfat <scottszb1987@hotmail.com>
2015-06-01 15:30:36 +1200

    V0.50
    -binary tree
    -pre-order traversal
    -in-order
    -post-order
    -pre-order with stack
    -in-order with stack
    -post-order with stack
    
    -using stack instead of recursion is a practical way, but kinda
    complicated
    -cannot figure out in-order and post-order using stack by myself without
    looking at tutor's node
    -took some time to understand tutor's node

---
+++ YuGeneric/UnitTestLList/UnitTestBinaryTree.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestBinaryTree
+    {
+        [TestMethod]
+        public void BinaryTreeNodeConstructor()
+        {
+            var node1 = new BinaryTreeNode<int>(0, null, null);
+            var node2 = new BinaryTreeNode<string>("0", null, null);
+        }
+
+        [TestMethod]
+        public void BinaryTreeNodeProperties()
+        {
+            var node1 = new BinaryTreeNode<int>(1, null, null);
+            var node2 = new BinaryTreeNode<int>(2, null, null);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+            Assert.AreEqual(node1.Value, 1);
+            Assert.ReferenceEquals(node2.Left, null);
+            Assert.ReferenceEquals(node0.Right, node2);
+
+            var node3 = new BinaryTreeNode<int>(3, null, null);
+            var node4 = new BinaryTreeNode<int>(4, null, null);
+            node1.Left = node3;
+            node2.Right = node4;
+            node4.Value = 5;
+            Assert.ReferenceEquals(node1.Left, node3);
+            Assert.AreEqual(node2.Right, node4);
+            Assert.AreEqual(node4.Value, 5);
+        }
+
+        [TestMethod]
+        public void BinaryTreeConstructor()
+        {
+            var tree0 = new BinaryTree<int>(0);
+            var tree1 = new BinaryTree<int>(new BinaryTreeNode<int>(1, null, null));
+        }
+
+        [TestMethod]
+        public void BinaryTreeRoot()
+        {
+            var node1 = new BinaryTreeNode<int>(1);
+            var node2 = new BinaryTreeNode<int>(2);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+            var tree0 = new BinaryTree<int>(node0);
+            Assert.AreEqual(tree0.Root.Value, 0);
+        }
+
+        [TestMethod]
+        public void BinaryTreeInOrder()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.InOrder(node0);
+        }
+
+        [TestMethod]
+        public void BinaryTreePreOrder()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.PreOrder(node0);
+        }
+
+        [TestMethod]
+        public void BinaryTreePostOrder()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.PostOrder(node0);
+        }
+
+        [TestMethod]
+        public void BinaryTreePreOrderWithStack()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.PreOrderWithStack(node0);
+        }
+
+        [TestMethod]
+        public void BinaryTreeInOrderWithStack()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.InOrderWithStack(node0);
+        }
+
+        [TestMethod]
+        public void BinaryTreePostOrderWithStack()
+        {
+            var node5 = new BinaryTreeNode<int>(5);
+            var node4 = new BinaryTreeNode<int>(4);
+            var node3 = new BinaryTreeNode<int>(3);
+            var node2 = new BinaryTreeNode<int>(2, node5, null);
+            var node1 = new BinaryTreeNode<int>(1, node3, node4);
+            var node0 = new BinaryTreeNode<int>(0, node1, node2);
+
+            BinaryTree<int>.PostOrderWithStack(node0);
+        }
+
+    }
+}
---
+++ YuGeneric/YuGeneric/BinaryTree.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGeneric
+{
+    public class BinaryTreeNode<T>
+    {
+        private T _value;
+        private BinaryTreeNode<T> _left;
+        private BinaryTreeNode<T> _right;
+
+        public BinaryTreeNode(T value)
+        {
+            _value = value;
+            _left = null;
+            _right = null;
+        }
+
+        public BinaryTreeNode(T value, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            _value = value;
+            _left = left;
+            _right = right;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public BinaryTreeNode<T> Left
+        {
+            get
+            {
+                return _left;
+            }
+            set
+            {
+                _left = value;
+            }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get
+            {
+                return _right;
+            }
+            set
+            {
+                _right = value;
+            }
+        }
+    }
+    
+    public class BinaryTree<T>
+    {
+        private BinaryTreeNode<T> _root;
+
+        public BinaryTree(BinaryTreeNode<T> root)
+        {
+            _root = root;
+        }
+
+        public BinaryTree(T rootValue)
+        {
+            _root = new BinaryTreeNode<T>(rootValue, null, null);
+        }
+
+        public BinaryTreeNode<T> Root
+        {
+            get
+            {
+                return _root;
+            }
+            set
+            {
+                _root = value;
+            }
+        }
+
+        public static void InOrder(BinaryTreeNode<T> node){
+            if (node == null) return;
+            InOrder(node.Left);
+            Console.Write(node.Value + " ");
+            InOrder(node.Right);
+        }
+
+        public static void PreOrder(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+            Console.Write(node.Value + " ");
+            PreOrder(node.Left);
+            PreOrder(node.Right);
+        }
+
+        public static void PostOrder(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+            PostOrder(node.Left);
+            PostOrder(node.Right);
+            Console.Write(node.Value + " ");
+        }
+
+        public static void PreOrderWithStack(BinaryTreeNode<T> node)
+        {
+            Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();
+            BinaryTreeNode<T> temp = node;
+            if (temp != null) stack.Push(temp);
+
+            while (!stack.IsEmpty())
+            {
+                temp = stack.Top.Value;
+                stack.Pop();
+                Console.Write(temp.Value + " ");
+                if (temp.Right != null)
+                {
+                    stack.Push(temp.Right);
+                }
+                if (temp.Left != null)
+                {
+                    stack.Push(temp.Left);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void InOrderWithStack(BinaryTreeNode<T> node)
+        {
+            var s = new Stack<BinaryTreeNode<T>>();
+            var temp = node;
+            while (temp != null)
+            {
+                while (temp != null)
+                {
+                    //--for 1st turn, temp is the root
+                    //--for rest turns, temp was the right child from last turn
+                    //--now seen as a sub root
+                    //--if it has childs, push right child first
+                    //--then himself back to stack
+                    //--repeatly do this until reach most left child in this branch
+                    if (temp.Right != null) {
+                        s.Push(temp.Right);
+                    }
+                    s.Push(temp);
+                    temp = temp.Left;
+                }
+
+                //Pop a node from the stack, hold on temp
+                temp = s.Top.Value;
+                s.Pop();
+
+                //print temp, update temp to a new poped node
+                //untill reach a node with right child
+                //temp hold this node, stack Top hold its right child
+                while (!s.IsEmpty() && temp.Right == null)
+                {
+                    Console.Write(temp.Value + " ");
+                    temp = s.Top.Value;
+                    s.Pop();
+                }
+
+                //print that node
+                Console.Write(temp.Value + " ");
+
+                //pop the right child node from the stack and give to temp
+                if (!s.IsEmpty())
+                {
+                    temp = s.Top.Value;
+                    s.Pop();
+                }
+                else
+                {
+                    temp = null;
+                }
+            }
+        }
+
+        public static void PostOrderWithStack(BinaryTreeNode<T> node){
+            var s = new Stack<BinaryTreeNode<T>>();
+            var curr = node;
+            var prev = node;
+
+            while (curr != null)
+            {
+                while (curr.Left != null)
+                {
+                    s.Push(curr);
+                    curr = curr.Left;
+                }
+
+                while (curr.Right == null || curr.Right == prev)
+                {
+                    Console.Write(curr.Value + " ");
+                    prev = curr;
+                    if (s.IsEmpty()) return;
+                    curr = s.Top.Value;
+                    s.Pop();
+                }
+
+                s.Push(curr);
+                curr = curr.Right;
+            }
+            Console.WriteLine();
+        }
+    }
+}
