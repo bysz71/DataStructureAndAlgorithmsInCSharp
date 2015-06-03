82228b2b7663cbecdbce56331068006ebe573ad5
feedfedfat <scottszb1987@hotmail.com>
2015-06-03 15:22:19 +1200

    V0.60
    -un-generic type BinarySearchTree added
    -Insert() implemented

---
+++ YuGeneric/UnitTestLList/UnitTestBinarySearchTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestBinarySearchTree
+    {
+        [TestMethod]
+        public void BSTInsert()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            BinarySearchTree.Insert(root, 8);
+            BinarySearchTree.Insert(root,20);
+            BinarySearchTree.Insert(root,10);
+        }
+
+        [TestMethod]
+        public void BSTInOrder()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            BinarySearchTree.Insert(root, 8);
+            BinarySearchTree.Insert(root, 20);
+            BinarySearchTree.Insert(root, 10);
+            BinarySearchTree.InOrderPrint(root);
+        }
+    }
+}
---
+++ YuGeneric/YuGeneric/BinarySearchTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGeneric
+{
+    public class BinarySearchTree
+    {
+        public static void Insert(TreeNodeInt root, int item)
+        {
+            if (item > root.Value)
+            {
+                if (root.Right == null)
+                {
+                    root.Right = new TreeNodeInt(item, null, null);
+                }
+                else
+                {
+                    Insert(root.Right, item);
+                }
+            }
+            else if (item < root.Value)
+            {
+                if (root.Left == null)
+                {
+                    root.Left = new TreeNodeInt(item, null, null);
+                }
+                else
+                {
+                    Insert(root.Left, item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: key is no unique");
+                return;
+            }
+        }
+
+        public static void InOrderPrint(TreeNodeInt root)
+        {
+            if (root == null) return;
+            InOrderPrint(root.Left);
+            Console.Write(root.Value + " ");
+            InOrderPrint(root.Right);
+        }
+    }
+
+    public class TreeNodeInt
+    {
+        public int Value;
+        public TreeNodeInt Left;
+        public TreeNodeInt Right;
+
+        public TreeNodeInt(int value, TreeNodeInt left, TreeNodeInt right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+
+}
