c18fde40420824a07e7deaa6c5cc7c83edb33ff9
feedfedfat <scottszb1987@hotmail.com>
2015-06-09 15:07:33 +1200

    V0.61
    -Binary Search Tree
    -new and changed:
    -Insert() -a recursive way
    -InsertNonRecursive() -a non-recursive way
    -InOrderPrint() - use in-order traversal to print the tree from smallest
    to largest
    -Search() -a non-recursive way
    -SearchRecursive() - a recursive way
    -Delete() -delete a node, based on 3 cases
    -DeleteLeaf() -private method, serves Delete(), delete a node without
    child
    -DeleteSingle() -private method, serves Delete(), delete a node with 1
    child
    -DeleteDouble() -private method, serves Delete(), delete a node with 2
    children
    -FindLeftLargest() -private method, serves DeleteDouble()
    -FindRightSmallest() -private method, serves DeleteDouble()
    -GetHeight() -private method, serves DeleteDouble()
    -FindMother() -private method,serves Delete()
    -Binary search tree basically done
    
    -been sick for a week

--- YuGeneric/UnitTestLList/UnitTestBinarySearchTree.cs
+++ YuGeneric/UnitTestLList/UnitTestBinarySearchTree.cs
@@ -7,23 +7,81 @@ namespace UnitTestYuGeneric
     [TestClass]
     public class UnitTestBinarySearchTree
     {
+        public void MakeTree(TreeNodeInt root)
+        {
+            BinarySearchTree.Insert(root, 4);
+            BinarySearchTree.Insert(root, 8);
+            BinarySearchTree.Insert(root, 20);
+            BinarySearchTree.Insert(root, 12);
+            BinarySearchTree.Insert(root, 3);
+            BinarySearchTree.Insert(root, 7);
+            BinarySearchTree.Insert(root, 17);
+        }
+
         [TestMethod]
         public void BSTInsert()
         {
             var root = new TreeNodeInt(10, null, null);
             BinarySearchTree.Insert(root, 8);
-            BinarySearchTree.Insert(root,20);
-            BinarySearchTree.Insert(root,10);
+            BinarySearchTree.Insert(root, 20);
+            BinarySearchTree.Insert(root, 10);
         }
 
         [TestMethod]
         public void BSTInOrder()
         {
             var root = new TreeNodeInt(10, null, null);
-            BinarySearchTree.Insert(root, 8);
-            BinarySearchTree.Insert(root, 20);
-            BinarySearchTree.Insert(root, 10);
+            MakeTree(root);
             BinarySearchTree.InOrderPrint(root);
         }
+
+        [TestMethod]
+        public void BSTSearch()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            MakeTree(root);
+            Assert.AreEqual(BinarySearchTree.Search(root, 12).Value, 12);
+            Assert.AreEqual(BinarySearchTree.Search(root, 10).Value, 10);
+            Assert.ReferenceEquals(BinarySearchTree.Search(root, 222), null);
+        }
+
+        [TestMethod]
+        public void BSTSearchRecursive()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            MakeTree(root);
+            Assert.AreEqual(BinarySearchTree.SearchRecursive(root, 12).Value, 12);
+            Assert.AreEqual(BinarySearchTree.SearchRecursive(root, 10).Value, 10);
+            Assert.ReferenceEquals(BinarySearchTree.SearchRecursive(root, 222), null);
+        }
+
+        [TestMethod]
+        public void BSTInsertNonRecursive()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            BinarySearchTree.InsertNonRecursive(root, 12);
+            BinarySearchTree.InsertNonRecursive(root, 20);
+            BinarySearchTree.InsertNonRecursive(root, 5);
+            BinarySearchTree.InsertNonRecursive(root, 10);
+            BinarySearchTree.InOrderPrint(root);
+        }
+
+        [TestMethod]
+        public void BSTDelete()
+        {
+            var root = new TreeNodeInt(10, null, null);
+            MakeTree(root);
+            BinarySearchTree.Delete(ref root, 4);
+            Assert.AreEqual(root.Left.Value, 7);
+            BinarySearchTree.Insert(root, 40);
+            BinarySearchTree.Insert(root, 30);
+            BinarySearchTree.Insert(root, 50);
+            BinarySearchTree.Insert(root, 35);
+            BinarySearchTree.Delete(ref root, 40);
+            Assert.AreEqual(BinarySearchTree.Search(root, 35).Left.Value, 30);
+            Assert.AreEqual(BinarySearchTree.Search(root, 35).Right.Value, 50);
+            Assert.ReferenceEquals(BinarySearchTree.Search(root,20).Right, BinarySearchTree.Search(root, 35));
+        }
+
     }
 }
--- YuGeneric/YuGeneric/BinarySearchTree.cs
+++ YuGeneric/YuGeneric/BinarySearchTree.cs
@@ -8,6 +8,14 @@ namespace YuGeneric
 {
     public class BinarySearchTree
     {
+        /// <summary>
+        /// Insert an int to a binary search tree
+        /// It uses recursive to implement insertion
+        /// It always add a new node to a leaf
+        /// It never breaks a existed connection to break itself in
+        /// </summary>
+        /// <param name="root">the root node of the BST</param>
+        /// <param name="item">the int that will be inserted</param>
         public static void Insert(TreeNodeInt root, int item)
         {
             if (item > root.Value)
@@ -39,6 +47,10 @@ namespace YuGeneric
             }
         }
 
+        /// <summary>
+        /// Just print out the tree use InOrder traversal
+        /// </summary>
+        /// <param name="root">tree root node</param>
         public static void InOrderPrint(TreeNodeInt root)
         {
             if (root == null) return;
@@ -46,8 +58,414 @@ namespace YuGeneric
             Console.Write(root.Value + " ");
             InOrderPrint(root.Right);
         }
+
+        /// <summary>
+        /// Search a specific value in a BST
+        /// Concept is kinda similar to Insert()
+        /// But not using recursive way
+        /// Which recursive can be used I think
+        /// I'll write a recursive search
+        /// </summary>
+        /// <param name="root">tree root node</param>
+        /// <param name="value">search key value</param>
+        /// <returns></returns>
+        public static TreeNodeInt Search(TreeNodeInt root, int value)
+        {
+            var current = root;
+            while (current != null)
+            {
+                if (current.Value == value)
+                {
+                    Console.WriteLine("Found {0}", value);
+                    return current;
+                }
+                else if (value < current.Value)
+                {
+                    current = current.Left;
+                }
+                else if (value > current.Value)
+                {
+                    current = current.Right;
+                }
+            }
+            Console.WriteLine("{0} is not found in the tree", value);
+            return null;
+        }
+
+        /// <summary>
+        /// Use recursive way to search a value
+        /// So yes recursive way can be used in here
+        /// Just same as insert
+        /// so insert() can be rewritten in non-recursive way I think
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static TreeNodeInt SearchRecursive(TreeNodeInt root, int value)
+        {
+            var current = root;
+            if (value == current.Value)
+            {
+                Console.WriteLine(value + " Found");
+            }else if (value < current.Value)
+            {
+                if (current.Left == null) current = null;
+                else current = SearchRecursive(current.Left, value);
+            }
+            else if (value > current.Value)
+            {
+                if (current.Right == null) current = null;
+                else current = SearchRecursive(current.Right, value);
+            }
+
+            if (current == null) Console.WriteLine(value + " not found in the BST");
+            return current;
+        }
+
+        /// <summary>
+        /// Insert a value using non-recursive way
+        /// So yeah, using loop can acheive insertion
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        public static void InsertNonRecursive(TreeNodeInt root, int value)
+        {
+            var current = root;
+            while (current != null)
+            {
+                if (value == current.Value)
+                {
+                    Console.WriteLine("Error: Un-Unique key not permitted.");
+                    return;
+                }
+
+                if (value < current.Value)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new TreeNodeInt(value, null, null);
+                        Console.WriteLine(value + " added");
+                        return;
+                    }
+                    else
+                    {
+                        current = current.Left;
+                        continue;
+                    }
+                }
+
+                if (value > current.Value)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new TreeNodeInt(value, null, null);
+                        Console.WriteLine(value + " added");
+                        return;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                        continue;
+                    }
+                }                        
+            }
+        }
+
+        /// <summary>
+        /// Delete a node when it is a leaf
+        /// A method serves Delete()
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static bool DeleteLeaf(ref TreeNodeInt root, int value)
+        {
+            var current = root;
+            if (root.Value == value)
+            {
+                Console.WriteLine("Error: You cannot delete the root of a one node tree");
+                return false;
+            }
+            var mother = FindMother(root, value);
+            if (value == mother.Left.Value)
+            {
+                mother.Left = null;
+                return true;
+            }
+            else
+            {
+                mother.Right = null;
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Delete a node when it has 1 child
+        /// A method serves Delete()
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static bool DeleteSingle(ref TreeNodeInt root, int value)
+        {
+            var current = root;
+            
+            if (root.Value == value)
+            {
+                if (root.Left != null)  root = root.Left;
+                else root = root.Right;
+                return true;
+            }
+
+            var mother = FindMother(root, value);
+            if (mother.Left.Value == value)
+            {
+                if (mother.Left.Left != null) mother.Left = mother.Left.Left;
+                else mother.Left = mother.Left.Right;
+            }
+            else
+            {
+                if (mother.Right.Left != null) mother.Right = mother.Right.Left;
+                else mother.Right = mother.Right.Right;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// A mothod serves Delete()
+        /// Delete a node when it has 2 children
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static bool DeleteDouble(ref TreeNodeInt root, int value)
+        {
+            if (root.Value == value)
+            {
+                if (GetHeight(root.Left) > GetHeight(root.Right))
+                {
+                    root = FindLeftLargest(root);
+                }
+                else
+                {
+                    root = FindRightSmallest(root);
+                }
+                return true;
+            }
+
+            var mother = FindMother(root, value);
+            if (mother.Left.Value == value)
+            {
+                if (GetHeight(mother.Left.Left) > GetHeight(mother.Left.Right))
+                {
+                    mother.Left = FindLeftLargest(mother.Left);
+                    Console.WriteLine("FindLeftLargest");
+                }
+                else
+                {
+                    mother.Left = FindRightSmallest(mother.Left);
+                    Console.WriteLine("FindRightLargest");
+                }
+            }
+            else
+            {
+                if (GetHeight(mother.Right.Left) > GetHeight(mother.Right.Right))
+                {
+                    mother.Right = FindLeftLargest(mother.Right);
+                    Console.WriteLine("FindLeftLargest");
+                }
+                else
+                {
+                    mother.Right = FindLeftLargest(mother.Right);
+                    Console.WriteLine("FindRightLargest");
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// A method serves Delete()
+        /// Find left largest node, break its original connections, put it on top
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static TreeNodeInt FindLeftLargest(TreeNodeInt node)
+        {
+            var current = node.Left;
+            var prev = current;
+            if (current.Right == null)
+            {
+                current.Right = node.Right;
+            }
+            else
+            {
+                while (current.Right != null)
+                {
+                    prev = current;
+                    current = current.Right;
+                }
+                //2 cases
+                //1, left largest node has no child
+                if (current.Left == null)
+                {
+                    prev.Right = null;
+                    current.Left = node.Left;
+                    current.Right = node.Right;
+                }
+                //2, left largest node has node (a left child)
+                else
+                {
+                    prev.Right = current.Left;
+                    current.Left = node.Left;
+                    current.Right = node.Right;
+                }
+            }
+            return current;
+        }
+
+        private static int GetHeight(TreeNodeInt node)
+        {
+            if (node.Left == null && node.Right == null)
+            {
+                return 1;
+            }
+            else
+            {
+                if (node.Left == null)
+                {
+                    return GetHeight(node.Right) + 1;
+                }
+                else if (node.Right == null)
+                {
+                    return GetHeight(node.Left) + 1;
+                }
+                else
+                {
+                    if (GetHeight(node.Left) >= GetHeight(node.Right))
+                    {
+                        return GetHeight(node.Left) + 1;
+                    }
+                    else
+                    {
+                        return GetHeight(node.Right) + 1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// A method serves Delete()
+        /// Find right smallest node, break its original connections, put it on top
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static TreeNodeInt FindRightSmallest(TreeNodeInt node)
+        {
+            var current = node.Right;
+            var prev = current;
+            if (current.Left == null)
+            {
+                current.Left = node.Left;
+            }
+            else
+            {
+                while (current.Left != null)
+                {
+                    prev = current;
+                    current = current.Left;
+                }
+                if (current.Right == null)
+                {
+                    prev.Left = null;
+                    current.Left = node.Left;
+                    current.Right = node.Right;
+                }
+                else
+                {
+                    prev.Left = current.Right;
+                    current.Left = node.Left;
+                    current.Right = node.Right;
+                }
+            }
+            return current;
+        }
+
+        /// <summary>
+        /// Find wanted value's mother node
+        /// A method serves Delete()
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static TreeNodeInt FindMother(TreeNodeInt root, int value)
+        {
+            var current = root;
+            while (true)
+            {
+                if (current.Left.Value == value || current.Right.Value == value)
+                {
+                    return current;
+                }
+                else
+                {
+                    if (value < current.Value)
+                    {
+                        current = current.Left;
+                    }
+                    else
+                    {
+                        current = current.Right;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Only open socket of delete
+        /// delete action depend on 3 different cases
+        /// 1.node has no child
+        /// 2.node has 1 child
+        /// 3.node has 2 children
+        /// 1st case is straight forward
+        /// 2st case use the child to replace itself
+        /// 3st case use the Left Largest node or Right Smallest node to replace itself depend on balance status
+        /// </summary>
+        /// <param name="root">root node of a tree, use "ref" keyword</param>
+        /// <param name="value">the node you want to delete has this value</param>
+        /// <returns></returns>
+        public static bool Delete(ref TreeNodeInt root, int value)
+        {
+            var search = Search(root, value);
+            var current = root;
+
+            if (search == null)
+            {
+                Console.WriteLine("No such item in the tree");
+                return false;
+            }
+            
+            if (search.Left == null && search.Right == null)
+            {
+                return DeleteLeaf(ref root, value);
+            }
+            else if (search.Left == null || search.Right == null)
+            {
+                return DeleteSingle(ref root, value);
+            }
+            else
+            {
+                return DeleteDouble(ref root, value);
+            }
+        }
     }
 
+    /// <summary>
+    /// a binary search tree node type, its value is an integer
+    /// did not use generic types to make the program easier
+    /// also because I don't know how to compare generic types
+    /// </summary>
     public class TreeNodeInt
     {
         public int Value;
@@ -61,5 +479,4 @@ namespace YuGeneric
             Right = right;
         }
     }
-
 }
