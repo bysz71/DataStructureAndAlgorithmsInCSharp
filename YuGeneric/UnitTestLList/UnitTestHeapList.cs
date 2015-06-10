e5dfe67244ca104d0c254be9d68007821e3db150
feedfedfat <scottszb1987@hotmail.com>
2015-06-10 14:11:48 +1200

    V0.62
    -heap implemented using vector (c# list)
    -heap is a self balanced tree, heights differs never 0-1
    -parent value is always larger than children's
    -good performance at insert and delete and search
    -using List makes it faster because random accessing
    
    -Insert()
    -DeleteRoot()
    -Print()

---
+++ YuGeneric/UnitTestLList/UnitTestHeapList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YuGeneric;
+using System.Collections.Generic;
+
+namespace UnitTestYuGeneric
+{
+    [TestClass]
+    public class UnitTestHeapList
+    {
+        public void HeapListInsertALot(HeapList heap)
+        {
+            heap.Insert(10);
+            heap.Insert(20);
+            heap.Insert(5);
+            heap.Insert(0);
+            heap.Insert(28);
+            heap.Insert(3);
+            heap.Insert(15);
+        }
+
+        [TestMethod]
+        public void HeapListConstructor()
+        {
+            var heap = new HeapList();
+        }
+
+        [TestMethod]
+        public void HeapListInsert()
+        {
+            var heap = new HeapList();
+            heap.Insert(10);
+            heap.Insert(20);
+            heap.Insert(5);
+            heap.Insert(5);
+        }
+
+        [TestMethod]
+        public void HeapListPrint()
+        {
+            var heap = new HeapList();
+            HeapListInsertALot(heap);
+            heap.Print();
+        }
+
+        [TestMethod]
+        public void HeapListDelete()
+        {
+            var heap = new HeapList();
+            HeapListInsertALot(heap);
+            heap.Print();
+            heap.DeleteRoot();
+            heap.Print();
+            heap.DeleteRoot();
+            heap.Print();
+        }
+
+        [TestMethod]
+        public void HeapListMaxIndex()
+        {
+            var heap = new HeapList();
+            HeapListInsertALot(heap);
+            Console.WriteLine(heap.MaxIndex(1));
+            Console.WriteLine(heap.MaxIndex(3));
+            Console.WriteLine(heap.MaxIndex(5));
+        }
+    }
+}
