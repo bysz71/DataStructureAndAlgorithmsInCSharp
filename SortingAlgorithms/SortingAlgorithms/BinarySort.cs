76e4e205de97fa3ffc958153b8a798031e96518a
feedfedfat <scottszb1987@hotmail.com>
2015-09-28 23:37:10 +1300

    starting refactoring for new file structure

---
+++ SortingAlgorithms/SortingAlgorithms/BinarySort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class BinarySort
+    {
+        /// <summary>
+        /// Quick sort using Lomuto partition schemme
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        public static void QuickSort(int[] nums, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivot = Partition(nums, low, high);
+                QuickSort(nums, low, pivot - 1);
+                QuickSort(nums, pivot + 1, high);
+            }
+        }
+
+        //Lomuto partition scheme
+        private static int Partition(int[] nums, int low, int high)
+        {
+            int pivot = nums[high];
+            int i = low;
+            for (int j = low; j < high; j++)
+                if (nums[j] <= pivot)
+                    Swap(ref nums[i++], ref nums[j]);
+            Swap(ref nums[i], ref nums[high]);
+            return i;
+        }
+
+        //swap the value of 2 ints
+        private static void Swap(ref int left, ref int right)
+        {
+            int buff = left;
+            left = right;
+            right = buff;
+        }
+    }
+}
---
+++ SortingAlgorithms/UnitTestSortingAlgorithms/UnitTestBinarySort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms;
+
+namespace UnitTestSortingAlgorithms
+{
+    [TestClass]
+    public class UnitTestBinarySort
+    {
+        [TestMethod]
+        public void TestMethodQuickSort()
+        {
+            var nums = new int[] { 3, 9, 2, 5, 6, 4, 11, 7, 12 };
+            BinarySort.QuickSort(nums, 0, nums.Length - 1);
+            CollectionAssert.AreEqual(new int[] { 2, 3, 4, 5, 6, 7, 9, 11, 12 }, nums);
+        }
+    }
+}
